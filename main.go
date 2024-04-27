cd22b2882089d2e8c100eaf6712773a234a51d2d
Asequesto <126966277+Asequesto@users.noreply.github.com>
2024-04-27 22:52:55 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	username = "postgres"
+	password = "postgres"
+	hostname = "postgres"
+	port     = 5432
+	db       = "postgres"
+)
+
+type Task struct {
+	ID        int64
+	Name      string
+	Completed bool
+}
+
+func createTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS test (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT FALSE
+  )
+  `
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateTask(db *sql.DB, task Task) error {
+	var result sql.Result
+	var err error
+	query := "INSERT INTO tasks (name, completed) VALUES ($1, $2)"
+
+	result, err = db.Exec(query, task.Name, task.Completed)
+
+	if err != nil {
+		return fmt.Errorf("CreateTask: %w", err)
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("CreateTask: %w", err)
+	}
+
+	fmt.Printf("Rows affected: %d\n", affected)
+	return nil
+}
+
+func GetTask(db *sql.DB) ([]*Task, error) {
+	var (
+		id        int64
+		name      string
+		completed bool
+	)
+
+	result := make([]*Task, 0, 10)
+
+	rows, err := db.Query("SELECT id, name, completed FROM tasks")
+	if err != nil {
+		return nil, fmt.Errorf("GetTask: %w", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&id, &name, &completed); err != nil {
+			return nil, fmt.Errorf("GetTask: %w", err)
+		}
+		result = append(result, &Task{
+			ID:        id,
+			Name:      name,
+			Completed: completed,
+		})
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("GetTask: %w", err)
+	}
+	return result, nil
+}
+
+func GetTaskCount(db *sql.DB) (int, error) {
+	var result int
+	if err := db.QueryRow("SELECT count(*) FROM tasks").Scan(&result); err != nil {
+		return 0, err
+	}
+	return result, nil
+}
+
+func DeleteTasks(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM tasks")
+	return err
+}
+
+func main() {
+	DSN := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", username, password, hostname, port, db)
+	db, err := sql.Open("postgres", DSN)
+
+	if err != nil {
+		fmt.Println(err, "here i am")
+		return
+	}
+
+	if err = db.Ping(); err != nil {
+		fmt.Println(err, "here")
+		return
+	}
+
+	defer db.Close()
+	fmt.Println("Successfully connected to postgres")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	createTable(db)
+
+	addTask := Task{
+		Name:      "Exercise",
+		Completed: true,
+	}
+
+	err = CreateTask(db, addTask)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	count, err := GetTaskCount(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Tasks count: %d\n", count)
+
+	tasks, err := GetTask(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, task := range tasks {
+		fmt.Printf("ID: %d. Name: %s. Completed: %s.", task.ID, task.Name, task.Completed)
+	}
+
+	if err := DeleteTasks(db); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nAll commands were successfully completed")
+}
